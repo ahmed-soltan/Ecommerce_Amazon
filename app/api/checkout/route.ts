1d217f110d0a1935a94426280c9d574a3e5e9f9c
unknown <soa548277@gmail.com>
2024-05-01 18:10:49 +0300

    create order functionality and integrate stripe payment gateway

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,64 +5,57 @@ import { stripe } from "@/lib/stripe";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { getCurrentProfile } from "@/actions/getCurrentProfile";
 import { cartProductType } from "@/app/(root)/product/[productId]/_components/ProductContainerDetails";
-export const POST = async (req: Request)=>{
 
+export const POST = async (req: Request) => {
     try {
-        const body = await req.json()
+        const body = await req.json();
         const user = await getCurrentUser();
-        if(!user){
-            return null
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
         }
         const profile = await getCurrentProfile();
-        if(!profile){
-            return null
+        if (!profile) {
+            return NextResponse.json({ error: "Profile not found" }, { status: 404 });
         }
         
-        const {products , amount} = body
-
-
-
+        const { products, amount } = body;
 
         let stripeCustomer = await prisma.stripeCustomer.findUnique({
-            where:{
-                userId:user.id
+            where: {
+                userId: user.id
             },
-            select:{
-                stripeCustomerId:true
+            select: {
+                stripeCustomerId: true
             }
-        })
+        });
         
-        if(!stripeCustomer){
+        if (!stripeCustomer) {
             const customer = await stripe.customers.create({
-                email:user.email!
-            })
+                email: user.email!
+            });
 
-         stripeCustomer = await prisma.stripeCustomer.create({
-                data:{
-                    userId:user.id,
-                    stripeCustomerId:customer.id
+            stripeCustomer = await prisma.stripeCustomer.create({
+                data: {
+                    userId: user.id,
+                    stripeCustomerId: customer.id
                 }
-            })
-            
+            });
         }
 
-
-
         const order = await prisma.order.create({
-            data:{
-                amount:amount,
-                products:products,
-                deliveryStatus:false,
-                profileId:profile.id,
-                paymentStatus:"open"
+            data: {
+                amount: amount,
+                products: products,
+                deliveryStatus: false,
+                profileId: profile.id,
+                paymentStatus: "open"
             }
-        })
+        });
 
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             mode: "payment",
             line_items: products.map((item: cartProductType) => {
-                console.log(item)
                 return {
                     price_data: {
                         currency: "usd",
@@ -79,20 +72,16 @@ export const POST = async (req: Request)=>{
             metadata: {
                 // Include any metadata you need here
                 profileId: profile.id,
-                orderId:order.id
+                orderId: order.id
             }
         });
 
-
-
-
-        console.log("SESSION : ",session)
-        console.log("ORDER : ",order)
+        console.log("SESSION : ", session);
+        console.log("ORDER : ", order);
         
-
-        return NextResponse.json({url:session.url})
+        return NextResponse.json({ url: session.url });
     } catch (error) {
-        console.error(error)
-        return NextResponse.json(error)
+        console.error(error);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
     }
-}
+};
