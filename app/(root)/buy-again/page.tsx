9806710d32f08a2dad882a2782c61a6daefa1b6f
ahmed-soltan <soa548277@gmail.com>
2024-11-03 13:39:39 +0200

    fix an issue

--- app/(root)/buy-again/page.tsx
+++ app/(root)/buy-again/page.tsx
@@ -1,23 +1,19 @@
-import { getCurrentProfile } from "@/actions/getCurrentProfile"
-import Container from "@/components/Container"
-import { redirect } from "next/navigation"
-import BuyAgainProducts from "./_components/BuyAgainProducts"
-import { Separator } from "@/components/ui/separator"
+import { getCurrentProfile } from "@/actions/getCurrentProfile";
+import Container from "@/components/Container";
+import { redirect } from "next/navigation";
+import BuyAgainProducts from "./_components/BuyAgainProducts";
+import { Separator } from "@/components/ui/separator";
 
-const BuyAgainPage = async() => {
-    const profile = await getCurrentProfile()
-    if(!profile){
-        return redirect('/login')
-    }
+const BuyAgainPage = async () => {
   return (
     <div className="p-6 bg-white">
-        <Container>
-            <h1 className="text-3xl text-slate-900 font-medium">Bug Again</h1>
-            <Separator className="my-5"/>
-            <BuyAgainProducts orders={profile.Order}/>
-        </Container>
+      <Container>
+        <h1 className="text-3xl text-slate-900 font-medium">Bug Again</h1>
+        <Separator className="my-5" />
+        <BuyAgainProducts />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default BuyAgainPage
+export default BuyAgainPage;
